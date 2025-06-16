4559cfe1b7a0b38db50b17fbae1cb99ccb007418
Philip Oyedoyin <maritine600@gmail.com>
2025-06-16 14:16:25 +0100

    Mod redis mocking

--- config/initializers/00_skip_redis.rb
+++ config/initializers/00_skip_redis.rb
@@ -33,4 +33,129 @@ if ENV['SKIP_REDIS_INITIALIZATION'] == 'true'
   # Replace the global Redis connections with mocks
   $alfred = MockConnectionPool.new
   $velma = MockConnectionPool.new
+end
+
+if ENV['RAILS_ENV'] == 'production' && ENV['SKIP_REDIS'] == 'true'
+  require 'redis'
+  require 'redis-namespace'
+  require 'connection_pool'
+
+  # Mock the Redis client at the lowest level
+  module RedisClient
+    class RubyConnection
+      def initialize(*args)
+        # Do nothing, prevent actual connection
+      end
+
+      def connect
+        # Do nothing, prevent actual connection
+      end
+
+      def connected?
+        true
+      end
+
+      def disconnect
+        # Do nothing
+      end
+
+      def call_v(command)
+        nil
+      end
+    end
+  end
+
+  # Mock Redis class
+  class Redis
+    def initialize(*args)
+      # Do nothing
+    end
+
+    def get(*args)
+      nil
+    end
+
+    def set(*args)
+      true
+    end
+
+    def del(*args)
+      true
+    end
+
+    def exists?(*args)
+      false
+    end
+
+    def keys(*args)
+      []
+    end
+
+    def flushdb
+      true
+    end
+
+    def quit
+      true
+    end
+
+    def close
+      true
+    end
+  end
+
+  # Mock Redis::Namespace
+  class Redis::Namespace
+    def initialize(*args)
+      # Do nothing
+    end
+
+    def get(*args)
+      nil
+    end
+
+    def set(*args)
+      true
+    end
+
+    def del(*args)
+      true
+    end
+
+    def exists?(*args)
+      false
+    end
+
+    def keys(*args)
+      []
+    end
+
+    def flushdb
+      true
+    end
+
+    def quit
+      true
+    end
+
+    def close
+      true
+    end
+  end
+
+  # Mock ConnectionPool
+  class ConnectionPool
+    def initialize(*args)
+      # Do nothing
+    end
+
+    def with
+      yield Redis.new
+    end
+  end
+
+  # Override Redis.new to return our mock
+  def Redis.new(*args)
+    Redis::Namespace.new('chatwoot', redis: Redis.new)
+  end
 end 
